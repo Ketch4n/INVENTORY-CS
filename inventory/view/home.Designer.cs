194d765b7486e04b07d2f8fa3189b145b0e8d6e4
Ketch4n <mangao.christian.04@gmail.com>
2024-03-19 13:16:05 +0800

    Save

--- inventory/view/home.Designer.cs
+++ inventory/view/home.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(home));
-            this.label1 = new System.Windows.Forms.Label();
             this.tab = new System.Windows.Forms.TabControl();
             this.dash = new System.Windows.Forms.TabPage();
             this.label2 = new System.Windows.Forms.Label();
@@ -40,45 +39,33 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.tab.SuspendLayout();
             this.dash.SuspendLayout();
             this.stock.SuspendLayout();
             this.transac.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("LEMON MILK Bold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.label1.Location = new System.Drawing.Point(9, 14);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 19);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "INVENTORY";
             // 
             // tab
             // 
+            this.tab.Appearance = System.Windows.Forms.TabAppearance.Buttons;
             this.tab.Controls.Add(this.dash);
             this.tab.Controls.Add(this.stock);
             this.tab.Controls.Add(this.transac);
-            this.tab.Location = new System.Drawing.Point(142, -5);
+            this.tab.Location = new System.Drawing.Point(109, 3);
             this.tab.Name = "tab";
             this.tab.SelectedIndex = 0;
-            this.tab.Size = new System.Drawing.Size(899, 658);
+            this.tab.Size = new System.Drawing.Size(928, 647);
             this.tab.TabIndex = 0;
             // 
             // dash
             // 
-            this.dash.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.dash.BackColor = System.Drawing.Color.White;
             this.dash.Controls.Add(this.label2);
-            this.dash.Location = new System.Drawing.Point(4, 22);
+            this.dash.Location = new System.Drawing.Point(4, 25);
             this.dash.Name = "dash";
             this.dash.Padding = new System.Windows.Forms.Padding(3);
-            this.dash.Size = new System.Drawing.Size(891, 632);
+            this.dash.Size = new System.Drawing.Size(920, 618);
             this.dash.TabIndex = 0;
             this.dash.Text = "DASHBOARD";
             // 
@@ -94,12 +81,12 @@
             // 
             // stock
             // 
-            this.stock.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.stock.BackColor = System.Drawing.Color.White;
             this.stock.Controls.Add(this.label4);
-            this.stock.Location = new System.Drawing.Point(4, 22);
+            this.stock.Location = new System.Drawing.Point(4, 25);
             this.stock.Name = "stock";
             this.stock.Padding = new System.Windows.Forms.Padding(3);
-            this.stock.Size = new System.Drawing.Size(891, 632);
+            this.stock.Size = new System.Drawing.Size(920, 568);
             this.stock.TabIndex = 1;
             this.stock.Text = "STOCKS";
             // 
@@ -115,12 +102,12 @@
             // 
             // transac
             // 
-            this.transac.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.transac.BackColor = System.Drawing.Color.White;
             this.transac.Controls.Add(this.label3);
-            this.transac.Location = new System.Drawing.Point(4, 22);
+            this.transac.Location = new System.Drawing.Point(4, 25);
             this.transac.Name = "transac";
             this.transac.Padding = new System.Windows.Forms.Padding(3);
-            this.transac.Size = new System.Drawing.Size(891, 632);
+            this.transac.Size = new System.Drawing.Size(920, 609);
             this.transac.TabIndex = 2;
             this.transac.Text = "TRANSACTIONS";
             // 
@@ -142,7 +129,7 @@
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button3.Location = new System.Drawing.Point(34, 209);
+            this.button3.Location = new System.Drawing.Point(20, 160);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(73, 71);
             this.button3.TabIndex = 5;
@@ -158,7 +145,7 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
-            this.button2.Location = new System.Drawing.Point(34, 132);
+            this.button2.Location = new System.Drawing.Point(20, 83);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(73, 71);
             this.button2.TabIndex = 4;
@@ -175,23 +162,26 @@
             this.button1.Font = new System.Drawing.Font("Montserrat", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.button1.Location = new System.Drawing.Point(34, 54);
+            this.button1.Location = new System.Drawing.Point(20, 5);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(73, 72);
             this.button1.TabIndex = 3;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
-            // pictureBox1
+            // button4
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = global::inventory.Properties.Resources._32x32;
-            this.pictureBox1.Location = new System.Drawing.Point(93, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(44, 40);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.button4.BackgroundImage = global::inventory.Properties.Resources._64x64;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(20, 569);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(73, 71);
+            this.button4.TabIndex = 6;
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // home
             // 
@@ -200,12 +190,11 @@
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1043, 652);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.tab);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -221,15 +210,11 @@
             this.stock.PerformLayout();
             this.transac.ResumeLayout(false);
             this.transac.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TabControl tab;
         private System.Windows.Forms.TabPage dash;
         private System.Windows.Forms.TabPage stock;
@@ -240,5 +225,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
     }
 }
